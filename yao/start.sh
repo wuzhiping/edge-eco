92c403f6fb05fb7cb92febccf8308d3531ef4dcf
wuzhiping <wuzhiping@users.noreply.github.com>
2022-09-10 10:33:37 +0800

    yao

---
+++ yao/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DARP_HOST_IP=$(nslookup $APP_ID | grep "Address: " | awk '{print $2}' | tr -d 'addr:')
+
+echo $DAPR_HOST_IP
+
+dapr run --app-id $APP_ID --app-port $APP_PORT --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- yao start
