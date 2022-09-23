24018d1143c47d6eb074e2dd98ac36d5a449bca6
wuzhiping <wuzhiping@users.noreply.github.com>
2022-09-23 11:23:02 +0800

    header support

--- eco/C4/dapr.js
+++ eco/C4/dapr.js
@@ -1,4 +1,5 @@
 const DAPR = require("@dapr/dapr");
+const request = require("request");
 
 const http = new DAPR.DaprClient("localhost", "3500");
 module.exports = {
@@ -6,5 +7,14 @@ module.exports = {
     DaprClient : DAPR.DaprClient,
     HttpMethod : DAPR.HttpMethod,
     CommunicationProtocolEnum : DAPR.CommunicationProtocolEnum,
-      LogLevel : DAPR.LogLevel
+      LogLevel : DAPR.LogLevel,
+
+        invoke : function(node,api,method,payload,headers,cb){
+                     request( {
+                                  url: "http:\/\/"+http.daprHost+":"+http.daprPort+"/v1.0/invoke/"+node+"/method"+api,
+                               method: method || "POST",
+                              headers: headers || { },
+                                 json: payload
+                               }, cb);
+                 }
 };
