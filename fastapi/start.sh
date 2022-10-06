dbd3dab4aa4e7789a294b46744fb6b3e5118e2c1
wuzhiping <wuzhiping@users.noreply.github.com>
2022-10-06 23:51:24 +0800

    python

---
+++ fastapi/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export DARP_HOST_IP=$(nslookup $APP_ID | grep "Address: " | awk '{print $2}' | tr -d 'addr:')
+
+echo $DAPR_HOST_IP
+
+jupyter notebook --allow-root --ip=0.0.0.0 $PWD &
+
+dapr run --app-id $APP_ID --app-port $APP_PORT --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- uvicorn main:app --host 0.0.0.0 --reload
