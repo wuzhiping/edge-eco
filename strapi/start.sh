8e3d29b6e34b9aabdd23dd712171f27531e619a4
wuzhiping <wuzhiping@users.noreply.github.com>
2022-10-09 00:12:51 +0800

    Add files via upload
    strapi 4.4.3

---
+++ strapi/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DARP_HOST_IP=$(nslookup $APP_ID | grep "Address: " | awk '{print $2}' | tr -d 'addr:')
+
+echo $DAPR_HOST_IP
+
+dapr run --app-id $APP_ID --app-port 1337 --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- npm run develop
+#-- node bpm.js
+#pm2 start bpm.js -i 2
+#pm2 logs
