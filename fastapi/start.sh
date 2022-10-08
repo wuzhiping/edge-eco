fe8952a5398484e2745518eea95bcb976761544a
wuzhiping <wuzhiping@users.noreply.github.com>
2022-10-08 10:18:03 +0800

    Update start.sh

--- fastapi/start.sh
+++ fastapi/start.sh
@@ -5,4 +5,4 @@ echo $DAPR_HOST_IP
 
 jupyter notebook --allow-root --ip=0.0.0.0 $PWD &
 
-dapr run --app-id $APP_ID --app-port $APP_PORT --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- uvicorn main:app --host 0.0.0.0 --reload
+dapr run --app-id $APP_ID --app-port $APP_PORT --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- /bin/bash -c "uvicorn main:app --host 0.0.0.0 --reload "
