64702bb2736bbf6903bc236cd216e4725dcb7555
wuzhiping <wuzhiping@users.noreply.github.com>
2022-12-21 10:27:49 +0800

    Update main.py

--- dash/main.py
+++ dash/main.py
@@ -1,15 +1,14 @@
 import uvicorn
 from fastapi.middleware.wsgi import WSGIMiddleware
-from dashapp import create_dash_app
-
 from typing import Union
-
 from fastapi import FastAPI
 
 from routers.users import router as users_router
+from dashapp import create_dash_app
 
 app = FastAPI()
 
+app.include_router(users_router, prefix = "/users", tags=["users"])
 
 @app.get("/")
 def read_root():
@@ -22,8 +21,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 dash_app = create_dash_app(requests_pathname_prefix="/dash/")
 app.mount("/dash", WSGIMiddleware(dash_app.server))
-
-app.include_router(users_router, prefix = "/users", tags=["users"])
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, port=8000)
