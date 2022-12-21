289896ad3076807ffb186efc3f37a830c51f11ff
wuzhiping <wuzhiping@users.noreply.github.com>
2022-12-21 15:45:47 +0800

    Update start.sh

--- dash/start.sh
+++ dash/start.sh
@@ -5,4 +5,4 @@ echo $DAPR_HOST_IP
 
 jupyter lab --allow-root --ip=0.0.0.0 $PWD &
 
-dapr run --app-id $APP_ID --app-port $APP_PORT --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- python app.py
+dapr run --app-id $APP_ID --app-port $APP_PORT --dapr-http-port 3500 --components-path /dapr/components --config /dapr/config.yaml --placement-host-address placement:50005 -- /bin/bash -c "uvicorn main:app --host 0.0.0.0 --reload "
